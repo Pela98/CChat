7d68d79f92d6c0281804c5772339206a6133dc8c
Pela98 <lorenzo.pelagatti2@stud.unifi.it>
2019-10-17 20:18:09 +0200

    vbasic-1.1

--- test/ChatTest.cpp
+++ test/ChatTest.cpp
@@ -17,10 +17,12 @@ ASSERT_EQ(LL.getOtherUser().getName(),"Luca");
 }
 
 TEST(Chat,TestChatMethods){
+Message msg1 = (Message("1111111111","0000000000","Ciao","20/08/19 08:40"));
 Message msg = (Message("3331234567", "3390987654", "Ciao", "20/09/19 09:15"));
 Chat LL(Lorenzo,Luca);
 LL.addMessage(msg);
 ASSERT_EQ(LL.lastMessage(), msg);
+ASSERT_THROW(LL.addMessage(msg1), std::invalid_argument);
 ASSERT_THROW(LL.readMessage(2), std::out_of_range);
 ASSERT_THROW(LL.readMessage(0), std::out_of_range);
 }
